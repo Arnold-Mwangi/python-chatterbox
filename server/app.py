bae57d8b9a19097a15e2e4924f1c4ef385cc3969
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-09-23 05:16:44 +0300

    Completed

--- server/app.py
+++ server/app.py
@@ -13,14 +13,74 @@ CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
+@app.route('/')
+def index():
+    return '<h3> We send Messages</h3>'
 
-@app.route('/messages')
+
+@app.route('/messages', methods=['GET', 'POST'])
 def messages():
-    return ''
 
-@app.route('/messages/<int:id>')
+    messages =  Message.query.all()
+    if request.method == 'GET':
+        messages_serialized= [message.to_dict() for message in messages]
+
+        response = make_response(
+            messages_serialized,
+            200
+        )
+        return response
+        
+    elif request.method == 'POST':
+        data = request.json
+        new_message = Message(
+            body = data.get("body"),
+            username = data.get("username"))
+            
+        db.session.add(new_message)
+        db.session.commit()
+
+        message_dict = new_message.to_dict()
+
+        response = make_response(
+            jsonify(message_dict),
+            201
+        )
+
+        return response
+
+@app.route('/messages/<int:id>', methods = ['PATCH', 'DELETE'])
 def messages_by_id(id):
-    return ''
 
+    message = Message.query.get(id)
+
+    if request.method =='PATCH':
+        data = request.json
+
+        for attr, value in data.items():
+            setattr(message, attr, value)
+
+            db.session.add(message)
+            db.session.commit()
+
+            message_dict = message.to_dict()
+            response = make_response(jsonify(message_dict),
+            200)
+        return response
+
+    elif request.method =='DELETE':
+        db.session.delete(message)
+        db.session.commit()
+        
+        response_body = {
+            "delete_status":"successful",
+            "message":"message deleted"
+        }
+        
+        response = make_response(
+            jsonify(response_body),
+            200
+        )
+    return response
 if __name__ == '__main__':
     app.run(port=5555)
